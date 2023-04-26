dd5ed932a62fcdf01ef6b03ba8dbe248d52d1ace
Arkadiusz Florek <arekflo123@gmail.com>
2023-04-27 00:11:37 +0200

    #

--- JS/js_strona_oNas.js
+++ JS/js_strona_oNas.js
@@ -1,7 +1,22 @@
 // CHce zrobic kod jquery, który doda pewien styl do obiektu w sekcji o Nas kiedy hoveruje obiekt
+/*
+$(document).ready(function() {
+    $('.container-o-nas-sekcja').hover( function(){
+        $(this).addClass('.active');
+        $('.container-o-nas-sekcja').not(this).addClass('inactive');},
+        function(){
+        $(this).removeClass('.active');
+        $('.container-o-nas-sekcja').not(this).removeClass('inactive');
+    });
+});*/
+
 
 $(document).ready(function() {
-    $('.container-o-nas').hover{
-        $('.container-o-nas-sekcja').not
-    }
-})
+    $('.container-o-nas-sekcja').hover(function() {
+      $(this).addClass('active');
+      $('.container-o-nas-sekcja').not(this).addClass('inactive');
+    }, function() {
+      $(this).removeClass('active');
+      $('.container-o-nas-sekcja').not(this).removeClass('inactive');
+    });
+  });
