3f831247742912a178b880016f69c560ad33b3a0
Arkadiusz Florek <arekflo123@gmail.com>
2023-04-26 22:34:12 +0200

    #

---
+++ JS/js_strona_oNas.js
@@ -0,0 +1,7 @@
+// CHce zrobic kod jquery, który doda pewien styl do obiektu w sekcji o Nas kiedy hoveruje obiekt
+
+$(document).ready(function() {
+    $('.container-o-nas').hover{
+        $('.container-o-nas-sekcja').not
+    }
+})
