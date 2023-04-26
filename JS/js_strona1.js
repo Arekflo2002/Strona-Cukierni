140f765725618d4b686f856b5f399a456c9c156d
Arkadiusz Florek <arekflo123@gmail.com>
2023-04-27 01:54:35 +0200

    Ogarnianie powoli i sprzatanie

--- JS/js_strona1.js
+++ JS/js_strona1.js
@@ -1,19 +1,24 @@
 
-window.addEventListener('scroll', function(){
-    var myText = document.querySelectorAll('.container-strona1-title p');
 
-    myText.forEach(function(text){
-        var textPosition = text.getBoundingClientRect();
-        var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        if (textPosition.top <= windowHeight && textPosition.bottom >= 0)
+// Dzialanie funkcji: Dodaje funkcje skroll do mojego okna strony, która najpierw zbiera każdy napis w klasie "...-title" znaczniku "p"
+// Nastepnie iteruje petla i pobieram pozycje uzytkownika oraz wysokosc strony
+// Sprawdzam czy uzytkownik znajduje sie na pozycji, gdzie jest w stanie zoabczyc napisy 
+// Nastepnei nadaje style kazdemu z napisowi, mianowicie "przekladam" napis i zmieniam przenikalnosc na 0/1.
+window.addEventListener('scroll', function(){
+    var napisy = document.querySelectorAll('.container-strona1-title p');
+
+    napisy.forEach(function(napis){
+        var pozycja_klient = napis.getBoundingClientRect();
+        var pozycja_okna = window.innerHeight || document.documentElement.clientHeight;
+        if (pozycja_klient.top <= pozycja_okna && pozycja_klient.bottom >= 0)
         {
-            text.style.opacity = 1;
-            text.style.transform = 'translateY(0)';
+            napis.style.opacity = 1;
+            napis.style.transform = 'translateY(0)';
         }
         else
         {
-            text.style.opacity = 0;
-            text.style.transform = 'translateY(100px)';
+            napis.style.opacity = 0;
+            napis.style.transform = 'translateY(100px)';
         }
     });
 });
--- JS/js_strona_oNas.js
+++ JS/js_strona_oNas.js
@@ -1,15 +1,8 @@
 // CHce zrobic kod jquery, który doda pewien styl do obiektu w sekcji o Nas kiedy hoveruje obiekt
-/*
-$(document).ready(function() {
-    $('.container-o-nas-sekcja').hover( function(){
-        $(this).addClass('.active');
-        $('.container-o-nas-sekcja').not(this).addClass('inactive');},
-        function(){
-        $(this).removeClass('.active');
-        $('.container-o-nas-sekcja').not(this).removeClass('inactive');
-    });
-});*/
 
+// Wiec sprawa jest prosta, gdy którys z elementow o klasie "...-sekcja" jest shoverowany to 
+// dodaje styl do tego obiektu styl ".active", a dla reszty elementow dla tej klasy ustawiam styl ".inactive".
+// Gdy uztykownik przestaje hoverowac to usuwam dokladnie te same style tym obiektom
 
 $(document).ready(function() {
     $('.container-o-nas-sekcja').hover(function() {
